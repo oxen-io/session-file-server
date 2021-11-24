a20948c6b4a710a24300ad15df8137e95ef731c0
Jason Rhinelander <jason@imaginary.ca>
2021-11-24 13:06:15 -0400

    Flask 1.x compatibility

--- fileserver/web.py
+++ fileserver/web.py
@@ -4,6 +4,18 @@ import flask
 
 app = flask.Flask(__name__)
 
+# Monkey-patch app.get/post/etc. for Flask <2 compatibility
+if not hasattr(flask.Flask, 'post'):
+
+    def _add_flask_method(name):
+        def meth(self, rule: str, **options):
+            return self.route(rule, methods=[name.upper()], **options)
+
+        setattr(flask.Flask, name, meth)
+
+    for method in ('get', 'post', 'put', 'delete', 'patch'):
+        _add_flask_method(method)
+
 from . import logging  # noqa: F401, E402
 from . import routes  # noqa: F401, E402
 from . import cleanup  # noqa: F401, E402
