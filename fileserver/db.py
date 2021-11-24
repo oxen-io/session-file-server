66be5a80da5fc4f4d12e47165422afc2b41b0b81
Jason Rhinelander <jason@imaginary.ca>
2021-11-24 12:50:27 -0400

    Add missing file

---
+++ fileserver/db.py
@@ -0,0 +1,34 @@
+from . import config
+from .postfork import postfork
+from .web import app
+
+from flask import g
+from psycopg_pool import ConnectionPool
+from werkzeug.local import LocalProxy
+
+
+@postfork
+def pg_connect():
+    global psql_pool
+    psql_pool = ConnectionPool(
+        min_size=2, max_size=32, kwargs={**config.pgsql_connect_opts, "autocommit": True}
+    )
+    psql_pool.wait()
+
+
+def get_psql_conn():
+    if "psql" not in g:
+        g.psql = psql_pool.getconn()
+
+    return g.psql
+
+
+@app.teardown_appcontext
+def release_psql_conn(exception):
+    psql = g.pop("psql", None)
+
+    if psql is not None:
+        psql_pool.putconn(psql)
+
+
+psql = LocalProxy(get_psql_conn)
